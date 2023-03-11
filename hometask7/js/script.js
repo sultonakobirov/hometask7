66e710382b690badf82d65ccc2b1dfb417051ee8
Sulton Akobirov <102214317+sultonakobirov@users.noreply.github.com>
2023-03-11 10:21:51 +0500

    Add files via upload

---
+++ hometask7/js/script.js
@@ -0,0 +1,24 @@
+//task-1
+let word = ['Bob', 'Janeo', "Leo", 'Alexo', 'Dicko','Roma', 'Antony']
+let nameDelete = prompt("Input name")
+let finalView = nameDelete.replace(nameDelete[0], nameDelete[0].toLocaleUpperCase())// to prevent from error while typing lowercase letters
+if(word.includes(finalView) === true){
+    let outPut = word.splice(word.indexOf(finalView), 1) // deleting given element 
+   alert(outPut) // output deleted element 
+   alert(word) // output array 
+}else{
+    alert("Not found!")
+}
+
+//task2 
+let namesObject = {
+    nameOne: "Thomas",
+    nameTwo: "Marko",
+    nameThree: "Polo",
+    nameFour: "Andres",
+    nameFive: "Pablo"
+}
+let namesArray = ['Bob', 'Janeo', "Leo", 'Alexo', 'Dicko','Roma', 'Antony']
+let objectToArray = Object.values(namesObject) // getting values of the object and returning array containing only values of the object 
+namesArray = namesArray.concat(objectToArray) // concatonation of two arrays
+console.log(namesArray);
